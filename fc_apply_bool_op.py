ac5da8947785e69450bed711ca3e763e971dc2e7
jayanam <jayanam.games@gmail.com>
2020-05-31 14:51:26 +0200

    1.2.7.1 : Apply selected pending booleans

--- fc_apply_bool_op.py
+++ fc_apply_bool_op.py
@@ -5,28 +5,40 @@ from .utils.fc_bool_util import *
 
 class FC_ApplyBoolOperator(Operator):
     bl_idname = "object.apply_bool"
-    bl_label = "Apply Boolean"
-    bl_description = "Apply pending bool operators" 
+    bl_label = "Apply Selected Booleans"
+    bl_description = "Apply selected pending bool operators" 
     bl_options = {'REGISTER', 'UNDO'} 
-       
+
     @classmethod
-    def poll(cls, context):        
-        return len(context.selected_objects) > 0
+    def poll(cls, context):
+ 
+        for obj in context.view_layer.objects:
+            for modifier in obj.modifiers:
+                if modifier.name.startswith("FC_BOOL") and modifier.object in context.selected_objects:
+                    return True
          
     def execute(self, context):
         
+        obj2delete = []
         active_obj = bpy.context.view_layer.objects.active
               
         for obj in context.view_layer.objects:
             for modifier in obj.modifiers:
                 if modifier.name.startswith("FC_BOOL"):
-                    # API change 2.8: bpy.context.scene.objects.active = obj
-                    bpy.context.view_layer.objects.active = obj
-                    bpy.ops.object.modifier_apply(modifier=modifier.name)
 
-        bpy.context.view_layer.objects.active = active_obj
-        
+                    # API change 2.8: bpy.context.scene.objects.active = obj
+                    if modifier.object in context.selected_objects:
+                        bpy.context.view_layer.objects.active = obj
+                        bpy.ops.object.modifier_apply(modifier=modifier.name)
+
+                        if is_delete_after_apply():
+                            obj2delete.append(modifier.object)
+
+        bpy.ops.object.select_all(action='DESELECT')
+
         if is_delete_after_apply():
-            bpy.ops.object.delete()
-        
+            for obj in obj2delete:
+                obj.select_set(True)
+                bpy.ops.object.delete()    
+
         return {'FINISHED'}
